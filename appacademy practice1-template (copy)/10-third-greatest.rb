dc44e2cf227285b299000b0d43ce6224f94cb624
Denis Naughton <mjolinor75@hotmail.com>
2021-01-13 19:32:11 -0500

    10-third-greatest.rb

--- appacademy practice1-template (copy)/10-third-greatest.rb
+++ appacademy practice1-template (copy)/10-third-greatest.rb
@@ -5,6 +5,8 @@
 # Difficulty: medium.
 
 def third_greatest(nums)
+	nums = nums.sort.reverse
+	nums[2]
 end
 
 # These are tests to check that your code is working. After writing
@@ -22,3 +24,5 @@ puts(
   'third_greatest([2, 3, 7, 4]) == 3: ' +
   (third_greatest([2, 3, 7, 4]) == 3).to_s
 )
+
+p third_greatest([5, 3, 7])
